4fc74b9e2f0b0b5e70523ba29b9a287a264062ac
Siddharth Shukla <sks2971996@gmail.com>
2018-05-25 17:09:42 +0530

    Update digitrecogniser.py

--- digitrecogniser.py
+++ digitrecogniser.py
@@ -1,28 +1,28 @@
-from sklearn import datasets
-from sklearn.svm import SVC
+from sklearn import datasets #datasets imported from sklearn
+from sklearn.svm import SVC #SVC is for classifier
 from scipy import misc
 
 
-digits = datasets.load_digits()
-features = digits.data 
-labels = digits.target
+digits = datasets.load_digits() #digit datasets loaded for training
+features = digits.data #features stores matrix corresponding to different digits
+labels = digits.target #labels stores the digits
 
 clf = SVC(gamma = 0.001)
-clf.fit(features, labels)
+clf.fit(features, labels) #clf is trained on features and labels
 
 
-img = misc.imread("2.jpg")
-img = misc.imresize(img, (8,8))
-img = img.astype(digits.images.dtype)
-img = misc.bytescale(img, high=16, low=0)
+img = misc.imread("2.jpg") #loading image
+img = misc.imresize(img, (8,8)) #changing image to 64 pixels.We take 64 becuase the training data is also 64 pixels.
+img = img.astype(digits.images.dtype) #making img data type same as the digits.image datatype. img is unsigned type and digits.images is float type. So, the two should be made same.
+img = misc.bytescale(img, high=16, low=0) 
 
 
 x_test = []
 
 for eachRow in img:
 	for eachPixel in eachRow:
-		x_test.append(sum(eachPixel)/3.0)
+		x_test.append(sum(eachPixel)/3.0) #x_test is the 1D array to store pixels of the input image i.e same format as the features which contains 64 elements
 
 
 
-print(clf.predict([x_test]))
+print(clf.predict([x_test])) #predict the output
